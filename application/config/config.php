424f7b0774560373267234760cc2b4951ffe22b8
M Amirul Ilmi <amirulilmi20@gmail.com>
2025-09-03 00:56:35 +0700

    add

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/dipecut/';
+$config['base_url'] = 'http://localhost/e-cuti-kemenag/';
 
 /*
 |--------------------------------------------------------------------------
